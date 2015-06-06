b16396201f85f2ed74a8b9e2a128ce9552c5b99e
Ary Rabelo <aryrabelo@gmail.com>
2015-06-06 20:58:38 -0300

    ajustando rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Samurai::Engine.routes.draw do
   root :to => 'home#index'
   get '/links' => 'home#links'
   get '/seo_tags' => 'home#seotags'
-  match '/seo_tags/:key' => 'home#edit', :as => :edit_tag, via: [:get, :post]
+  match '/seo_tags/:key' => 'home#edit', :as => :edit_tag, via: [:get, :post, :patch]
 end
